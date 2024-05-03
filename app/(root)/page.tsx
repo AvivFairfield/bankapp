f02c1c8e44eb4e781232475c93c145a318692ea2
AvivFairfield <avivcohen22@icloud.com>
2024-05-03 03:52:14 +0300

    added pages

---
+++ app/(root)/page.tsx
@@ -0,0 +1,38 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
+const Home = () => {
+	const loggedIn = {
+		firstName: "Aviv",
+		lastName: "Cohen",
+		email: "avivcohen22@icloud.com",
+	};
+	return (
+		<section className="home">
+			<div className="home-content">
+				<header className="home-header">
+					<HeaderBox
+						type="greeting"
+						title="Welcome"
+						user={loggedIn?.firstName || "Guest"}
+						subtext="Access and manage your account and transactions"
+					/>
+					<TotalBalanceBox
+						accounts={[]}
+						totalBanks={1}
+						totalCurrentBalance={1250.35}
+					/>
+				</header>
+				RECENT TRANS
+			</div>
+			<RightSidebar
+				user={loggedIn}
+				transactions={[]}
+				banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+			/>
+		</section>
+	);
+};
+
+export default Home;
