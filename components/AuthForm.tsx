0de9f41afb89d9718d63dffbab71bb73867e087c
AvivFairfield <avivcohen22@icloud.com>
2024-05-04 23:58:44 +0300

    auth complete with appwrite

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,8 +20,11 @@ import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
+	const router = useRouter();
 	const [user, setUser] = useState(null);
 	const [isLoading, setIsLoading] = useState(false);
 
@@ -35,13 +38,20 @@ const AuthForm = ({ type }: { type: string }) => {
 		},
 	});
 
-	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+	const onSubmit = async (data: z.infer<typeof formSchema>) => {
 		setIsLoading(true);
 
 		try {
 			if (type === "sign-up") {
+				const newUser = await signUp(data);
+				setUser(newUser);
 			}
 			if (type === "sign-in") {
+				const response = await signIn({
+					email: data.email,
+					password: data.password,
+				});
+				if (response) router.push("/");
 			}
 		} catch (error) {
 			console.log(error);
@@ -115,6 +125,12 @@ const AuthForm = ({ type }: { type: string }) => {
 										name="address1"
 										label="Address"
 										placeholder="Enter your address"
+									/>
+									<CustomInput
+										control={form.control}
+										name="city"
+										label="City"
+										placeholder="Enter your city"
 									/>
 									<div className="flex gap-4">
 										<CustomInput
