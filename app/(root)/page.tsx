20c03cd35985f73de2cf4971031f0b1181317744
AvivFairfield <avivcohen22@icloud.com>
2024-05-06 00:16:15 +0300

    updated banks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,18 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
 	const loggedIn = await getLoggedInUser();
 
+	const accounts = await getAccounts({ userId: loggedIn.$id });
+	if (!accounts) return;
+
+	const accountsData = accounts?.data;
+	const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+	const account = await getAccount({ appwriteItemId });
 	return (
 		<section className="home">
 			<div className="home-content">
@@ -13,21 +20,21 @@ const Home = async () => {
 					<HeaderBox
 						type="greeting"
 						title="Welcome"
-						user={loggedIn?.name || "Guest"}
+						user={loggedIn?.firstName || "Guest"}
 						subtext="Access and manage your account and transactions"
 					/>
 					<TotalBalanceBox
-						accounts={[]}
-						totalBanks={1}
-						totalCurrentBalance={1250.35}
+						accounts={accountsData}
+						totalBanks={account?.totalBanks}
+						totalCurrentBalance={accounts?.totalCurrentBalance}
 					/>
 				</header>
 				RECENT TRANS
 			</div>
 			<RightSidebar
 				user={loggedIn}
-				transactions={[]}
-				banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+				transactions={account?.transactions}
+				banks={accountsData?.slice(0, 2)}
 			/>
 		</section>
 	);
